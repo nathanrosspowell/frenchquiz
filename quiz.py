0e9ef0c645ea3b0b8d565664653dc0cc84aa2dd4
Nathan Ross Powell <nathanrosspowell@gmail.com>
2013-03-09 22:17:46 -0500

    [MOD] Refactor the way that the quiz to be made
    Decorator to loop over the 'attempts' of answering the question
    Partial functions for final execution in the main quiz loop,
    where the 'trys' are passed in at the last possible time.

--- quiz.py
+++ quiz.py
@@ -4,6 +4,7 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Imports.
 from random import shuffle, randrange
+from functools import partial
 # Local imports.
 import words as frenchWords
 import verbs as frenchVerbs
@@ -13,40 +14,37 @@ import buildNumber
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Simpler input.
 def input( output ):
-    return raw_input( output ).strip()
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    return raw_input( output ).strip() #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Quiz a range.
-def quizRange( quizFunction, numberRange, trys = 3, shuffleThem = True ):
-    if shuffleThem:
-        shuffle( numberRange )
-    items = len( numberRange )
-    print "%s items in test:" % ( items, )
+def quiz( functionItems, numberRange, trys = 3 ):
+    numberItems = len( functionItems )
+    print "%s items in test:" % ( numberItems, )
     correct = 0
     totalAttempts = 0
-    firstQ = True
-    for i, number in enumerate( numberRange ):
-        if not firstQ:
-            print "--------------"
-        firstQ = False;
-        print "%s) " % ( i + 1, ),
-        answer, attempts = quizFunction( number, trys )
+    for i, functionItem in enumerate( functionItems ):
+        print "--------------\n%s) " % ( i + 1, ),
+        answer, attempts = functionItem( trys = trys )
         totalAttempts += attempts
         if answer:
             correct += 1
-    print "Score: %d/%d, total attempts: %d" % ( correct, items, totalAttempts, )
+    print "Score: %d/%d, total attempts: %d" % ( correct, numberItems, totalAttempts, )
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Quiz a range.
+def makeFunctionItems( quizFunction, items, shuffleThem = True ):
+    if shuffleThem:
+        shuffle( items )
+    return [ partial( quizFunction, x ) for x in items ]
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Quiz a number.
-def quizNumber( number, trys = 3 ):
-    word = getWord( number )
-    print "What number is '%s'" % ( word, )
-    answer = False
-    totalAttempts = 0
-    while trys > 0 and answer is False:
-        totalAttempts += 1
-        answerWord = input( "Answer> " )
-        try: 
-            answerNumber = int( answerWord )
-            if answerNumber == number:
+def tryLoopForItem( quizFunction ):
+    def decorator( *args, **kwargs ):
+        trys = kwargs.get( "trys", 1 )
+        totalAttempts = 0
+        answer = False
+        while trys > 0 and answer is False:
+            totalAttempts += 1
+            result, input = quizFunction( *args, **kwargs )
+            if result:
                 answer = True
                 print "Well done!"
             else:
@@ -54,36 +52,35 @@ def quizNumber( number, trys = 3 ):
                 print "Incorrect."
                 if trys > 0:
                     print "Please try again."
-        except:
-            print "Please entre a number."
-    print "'%s' is the number %d" % ( word, number, )
-    return answer, totalAttempts
+        return answer, totalAttempts
+    return decorator
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Quiz a number.
+@tryLoopForItem
+def quizNumber( number, trys = 3 ):
+    word = buildNumber.getWord( number )
+    print "What number is '%s'" % ( word, )
+    answerWord = input( "Answer> " )
+    answerNumber = int( answerWord )
+    try: 
+        answerNumber = int( answerWord )
+    except:
+        answerNumber = False
+    return answerNumber == number, answerNumber
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Quiz a word.
+@tryLoopForItem
 def quizWord( number, trys = 3 ):
     def cleanWord( theWord ):
         return theWord.replace( '-', ' ' ).strip()
     word = buildNumber.getWord( number )
     print "What is the word for '%d'" % ( number, )
-    answer = False
-    totalAttempts = 0
-    while trys > 0 and answer is False:
-        totalAttempts += 1
-        answerWord = cleanWord( input( "Answer> " ) )
-        if answerWord == cleanWord( word ):
-            answer = True
-            print "Well done!"
-        else:
-            trys -= 1
-            print "Incorrect."
-            if trys > 0:
-                print "Please try again."
-    print "'%s' is the number %d" % ( word, number, )
-    return answer, totalAttempts
+    answerWord = cleanWord( input( "Answer> " ) )
+    return answerWord == cleanWord( word ), answerWord
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # A bunch of number quiz functions
 def quizLowNumbers( quizFunction, trys = 3 ):
-    quizRange( quizFunction, range( 20 ), trys )
+    quiz( makeFunctionItems( quizFunction, range( 2 ) ), trys )
 def quizTensNumbers( quizFunction, trys = 3 ):
     quizRange( quizFunction, range( 20 ), trys )
 def quizRandom( quizFunction, maxNumber, total, trys = 3 ):
