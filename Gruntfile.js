527b078b59bd179eba68dc7761d1addcb13c5308
Nathan Ross Powell <nathanrosspowell@gmail.com>
2014-07-03 21:42:35 -0400

    Adding setup

---
+++ Gruntfile.js
@@ -0,0 +1,157 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+var fs = require('fs');
+var path = require('path');
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                    '<%= grunt.template.today("yyyy-mm-dd") %> */',
+                compress: {
+                    drop_console: false
+                }
+            },
+            frenchQuiz: {
+                files: {
+                    'build/js/main.js': ['src/js/main.js'],
+                    'build/js/scripts.js': ['src/js/scripts.js'],
+                }
+            }
+        },
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        copy: {
+            frenchQuiz: {
+                files: [{
+                    expand: true,
+                    cwd: "src/",
+                    src: [
+                        '**/*', // The full source tree, but...
+                        '!**/*.handlebars', // ignore handlebar files,
+                        '!**/js/main.js', // ignore this,
+                        '!**/js/scripts.js', // ignore this,
+                        '!**/yaml/*', // and ignore this folder.
+                    ],
+                    dest: 'build/',
+                    filter: "isFile"
+                }]
+            }
+        },
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        yaml: {
+            frenchQuiz: {
+                options: {
+                    space: 2
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/yaml/',
+                    src: ['**/*.yml'],
+                    dest: 'temp/json/'
+                }]
+            }
+        },
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        concat: {
+            frenchQuiz: {
+                options: {
+                    process: function(src, filepath) {
+                        var filename = filepath.replace(/^.*[\\\/]/, '')
+                        var withoutExt = filename.split('.')[0]
+                        return '"' + withoutExt + '": ' + src + ",";
+                    },
+                },
+                files: {
+                    'temp/concatfile.json': ['temp/json/*.json']
+                },
+            }
+        },
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        replace: {
+            frenchQuiz: {
+                options: {
+                    patterns: [{
+                        match: /\},$/g, // This is the last comma in the file
+                        replacement: "}",
+                    }]
+                },
+                files: {
+                    'temp/concatfile.json': 'temp/concatfile.json'
+                },
+            }
+        },
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        file_append: {
+            frenchQuiz: {
+                files: {
+                    'temp/built.json': {
+                        prepend: "{\n",
+                        append: "\n}\n",
+                        input: 'temp/concatfile.json'
+                    }
+                },
+            }
+        },
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        'compile-handlebars': {
+            frenchQuiz: {
+                template: 'src/**/*.handlebars',
+                templateData: 'temp/built.json',
+                output: 'build/**/*index.html'
+            }
+        },
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        prettify: {
+            frenchQuiz: {
+                files: [{
+                    expand: true,
+                    cwd: 'build/',
+                    src: ['**/*.html'],
+                    dest: 'build/'
+                }]
+            }
+        },
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        jsbeautifier: {
+            "frenchQuiz": {
+                src: [
+                    "Gruntfile.js",
+                    "src/main.js",
+                    "src/scripts.js",
+                ]
+            },
+        },
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        'gh-pages': {
+            frenchQuiz: {
+                options: {
+                    base: 'build'
+                },
+                src: ['**']
+            }
+        }
+    });
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Load tasks.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-yaml');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-file-append');
+    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.loadNpmTasks('grunt-compile-handlebars');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-prettify');
+    grunt.loadNpmTasks('grunt-jsbeautifier');
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Default task: does everything except deployment
+    grunt.registerTask('default', ['make-yaml', 'handlebars', 'uglify', 'copy']);
+    grunt.registerTask('make-yaml', ['yaml', 'concat', 'replace', 'file_append']);
+    grunt.registerTask('handlebars', ['compile-handlebars', 'prettify']);
+    // deploy task: runs everything in order.
+    grunt.registerTask('deploy', ['default', 'gh-pages']);
+};
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
