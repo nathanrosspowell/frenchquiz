18a5e83a7e5b2dfb6ad6331c7f92d7cae07b0678
Nathan Ross Powell <nathanrosspowell@gmail.com>
2014-07-05 00:58:17 -0400

    Updates

---
+++ helpers/each_quiz.js
@@ -0,0 +1,29 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+module.exports = function(options){
+    var result = '';
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // List all of the quiz YAML files.
+    var quizes = [
+        { 
+            name: "Basic Quiz",
+            id: "basic_quiz",
+            context: this.quiz
+        },
+        { 
+            name: "Next Quiz",
+            id: "next_quiz",
+            context: this.quiz2
+        }
+    ];
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Execute the options function on each object.
+	for (var i = 0; i < quizes.length; i++) {
+        var context = quizes[i].context;
+        context.name = quizes[i].name;
+        context.id = quizes[i].id;
+        context.index = i;
+        result += options.fn( context );
+	}
+	return result;
+};
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
